a7aebc72f46b9ecddb7433687529218efbab0e3f
Ameer Sorne <ameersorne@gmail.com>
2016-12-17 12:40:08 +0800

    fixed btnBack source code

--- src/com/neet/DiamondHunter/MainApp/MapViewerController.java
+++ src/com/neet/DiamondHunter/MainApp/MapViewerController.java
@@ -1,13 +1,12 @@
 package com.neet.DiamondHunter.MainApp;
 
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.awt.*;  
 
 public class MapViewerController {
 
@@ -16,7 +15,9 @@ private Button btnBack;
 
     @FXML
     public void exit()throws Exception{
-        //Platform.exit();
+        Scene scene = btnBack.getScene();
+        Stage test = (Stage)scene.getWindow();
+        test.hide();
     }
 
 }
