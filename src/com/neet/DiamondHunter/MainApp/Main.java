aff9783daa922f83455e6c6f848d34cbc920a175
Durai23 <khcy5dnr@nottingham.edu.my>
2016-12-17 10:47:21 +0800

    New changes

---
+++ src/com/neet/DiamondHunter/MainApp/Main.java
@@ -0,0 +1,29 @@
+package com.neet.DiamondHunter.MainApp;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	public void start(Stage primaryStage) {
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MapViewerMenu.fxml"));			
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Diamond Hunter");
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
