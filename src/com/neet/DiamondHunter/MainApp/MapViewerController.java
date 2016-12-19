521e68ddb51c0371bbbd2ac19758aefaf70a19fb
MizanQistina <inamizan@gmail.com>
2016-12-19 12:58:13 +0800

    Added save() to run SavedNotification

--- src/com/neet/DiamondHunter/MainApp/MapViewerController.java
+++ src/com/neet/DiamondHunter/MainApp/MapViewerController.java
@@ -5,7 +5,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -19,6 +21,9 @@ public class MapViewerController implements Initializable{
 	
 	@FXML
 	private Button btnBack;
+	
+	@FXML
+	private Button btnSave;
 	
 	@FXML
 	private Canvas mapCanvas;
@@ -46,5 +51,16 @@ public class MapViewerController implements Initializable{
         currentScene.hide();
     }
 
+	@FXML
+	public void save() throws Exception{    
+    	Stage stage = new Stage();
+    	Parent root = FXMLLoader.load(getClass().getResource("SavedNotification.fxml"));
+    	//stage.initStyle(StageStyle.UNDECORATED);
+    	stage.setTitle(" Save Complete ");
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("SavedNotification.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+    }
 
 }
